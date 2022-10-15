536d4830b4a9b1ab158edaffb3606a40926e64ab
KID <50666207+KID421@users.noreply.github.com>
2022-10-15 16:59:34 +0800

    Update 判斷式、迴圈、列舉與陣列

--- Unity_HC_S_HunterShot_20220730/Assets/Scripts/LearnChoose.cs
+++ Unity_HC_S_HunterShot_20220730/Assets/Scripts/LearnChoose.cs
@@ -6,22 +6,43 @@ namespace KID
     /// 選取語句：
     /// 1. if
     /// 2. switch
+    /// 3. 列舉 enumeration
     /// </summary>
     public class LearnChoose : MonoBehaviour
     {
         [SerializeField]
         private int keyCount;
+        [SerializeField]
+        private int score = 0;
+        [SerializeField]
+        private string weapon;
+
+        // 1. 定義列舉與選項
+        // 2. 將列舉儲存於欄位內
+        private enum Season
+        {
+            Spring, Summer, Fall, Winter
+        }
+
+        [SerializeField]
+        private Season gameSeason;
 
         private void Awake()
         {
+            gameSeason = Season.Fall;
+
             ChooseIf();
         }
 
         private void Update()
         {
             UpdateChooseIf();
+            UpdateScoreIf();
+            ChooseSwitch();
+            SeasonSwitch();
         }
 
+        #region if
         /// <summary>
         /// if
         /// </summary>
@@ -53,6 +74,74 @@ namespace KID
                 print("不過關");
             }
         }
+
+        /// <summary>
+        /// 分數：
+        /// 大於等於 60 分：及格
+        /// 低於 60 分：不及格
+        /// 低於 40 分：被當
+        /// </summary>
+        private void UpdateScoreIf()
+        {
+            // if () {}
+            // else if () {} - 必須放在 if 下方，else 上方 
+            // else {}
+            if (score >= 60)
+            {
+                print("及格");
+            }
+            else if (score >= 40)
+            {
+                print("不及格");
+            }
+            else
+            {
+                print("你被當了！");
+            }
+        }
+        #endregion
+
+        #region switch
+        private void ChooseSwitch()
+        {
+            // swithc (任何資料類型) { 選取語句 }
+            switch (weapon)
+            {
+                // 如果 武器 為 小刀 就執行 攻擊力 10 並且跳出
+                case "小刀":
+                    print("攻擊力 10");
+                    break;
+
+                case "手榴彈":
+                    print("攻擊力 50");
+                    break;
+
+                // 當上面的 case 都不符合執行，可省略
+                default:
+                    print("這是不能用的武器");
+                    break;
+            }
+        }
+
+        private void SeasonSwitch()
+        {
+            switch (gameSeason)
+            {
+                case Season.Spring:
+                    print("春天");
+                    break;
+                case Season.Summer:
+                    print("夏天");
+                    break;
+                case Season.Fall:
+                    print("秋天");
+                    break;
+                case Season.Winter:
+                    print("冬天");
+                    break;
+            }
+        }
+        #endregion
     }
 }
 
---
+++ Unity_HC_S_HunterShot_20220730/Assets/Scripts/LearnLoop.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 學習迴圈：反復語句
+    /// 1. while
+    /// 2. for
+    /// 3. 陣列 Array
+    /// </summary>
+    public class LearnLoop : MonoBehaviour
+    {
+        private int countWhile = 0;
+
+        [SerializeField] private int numberA = 10;
+        [SerializeField] private int numberB = 50;
+        [SerializeField] private int numberC = 500;
+
+        [SerializeField]
+        private int[] numbers;
+
+        private void Awake()
+        {
+            WhileBase();
+            ForBase();
+
+            // 取得陣列資料 Get
+            print("第二個數字的值：" + numbers[1]);
+
+            // 存放陣列資料 Set
+            numbers[2] = 99;
+        }
+
+        /// <summary>
+        /// while 迴圈基礎
+        /// </summary>
+        private void WhileBase()
+        {
+            // 當布林值為 true 時執行一次
+            if (true)
+            {
+
+            }
+
+            // 當布林值為 true 時持續執行
+            while (countWhile < 5)
+            {
+                print("while 迴圈執行次數：" + countWhile);
+                // countWhile = countWhile + 1;
+                countWhile++;
+            }
+        }
+
+        /// <summary>
+        /// for 迴圈基礎
+        /// </summary>
+        private void ForBase()
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                print("<color=yellow>for 迴圈執行次數：" + x + "</color>");
+            }
+        }
+    }
+}
